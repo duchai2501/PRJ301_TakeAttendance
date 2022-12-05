a3d6d35f325c8f2b2b2b879c0ce53bf369a89991
duchai2501 <99119719+duchai2501@users.noreply.github.com>
2022-12-05 23:57:18 +0700

    Update DBContext.java

--- Assignment/src/java/dal/DBContext.java
+++ Assignment/src/java/dal/DBContext.java
@@ -7,6 +7,7 @@ package dal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,16 +15,11 @@ import java.util.logging.Logger;
  *
  * @author admin
  */
-public class DBContext {
+public abstract class DBContext<T> {
+
     protected Connection connection;
-    public DBContext()
-    {
-        //@Students: You are allowed to edit user, pass, url variables to fit 
-        //your system configuration
-        //You can also add more methods for Database Interaction tasks. 
-        //But we recommend you to do it in another class
-        // For example : StudentDBContext extends DBContext , 
-        //where StudentDBContext is located in dal package, 
+
+    public DBContext() {
         try {
             String user = "hai";
             String pass = "123";
@@ -34,4 +30,10 @@ public class DBContext {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    public abstract void insert(T model); //INSERT INTO
+    public abstract void update(T model); // UPDATE SET
+    public abstract void delete(T model); // DELETE FROM 
+    public abstract T get(int id); // SELECT * FROM WHERE id = 
+    public abstract ArrayList<T> list(); //override SELECT * FROM
 }
