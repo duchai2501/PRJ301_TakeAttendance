1c0fe9b87978c800f0c5682aa1a81fe5498fca9f
duchai2501 <99119719+duchai2501@users.noreply.github.com>
2022-12-09 00:02:45 +0700

    Update Subject.java

--- Assignment/src/java/model/Subject.java
+++ Assignment/src/java/model/Subject.java
@@ -4,10 +4,50 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author admin
  */
 public class Subject {
+    private String id;
+    private String name;
+    private int slots;
+    private ArrayList<Group> groups = new ArrayList<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSlots() {
+        return slots;
+    }
+
+    public void setSlots(int slots) {
+        this.slots = slots;
+    }
+
+    public ArrayList<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(ArrayList<Group> groups) {
+        this.groups = groups;
+    }
+    
+    
     
 }
