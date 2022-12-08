3d738283093695b4e6278ccf2437eaf96579a985
duchai2501 <99119719+duchai2501@users.noreply.github.com>
2022-12-09 00:33:08 +0700

    Update Group.java

--- Assignment/src/java/model/Group.java
+++ Assignment/src/java/model/Group.java
@@ -4,10 +4,85 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author admin
  */
 public class Group {
-    
+
+    private int id;
+    private String name;
+    private Subject subject;
+    private Lecturer lecturer;
+    private ArrayList<Session> sessions = new ArrayList<>();
+    private ArrayList<Student> students = new ArrayList<>();
+    private String sem;
+    private int year;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Lecturer lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public ArrayList<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(ArrayList<Session> sessions) {
+        this.sessions = sessions;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public String getSem() {
+        return sem;
+    }
+
+    public void setSem(String sem) {
+        this.sem = sem;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
 }
