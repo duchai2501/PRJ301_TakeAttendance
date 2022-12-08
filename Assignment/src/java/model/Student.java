c462e0d4169cda554eb03303470763b7b493f2d8
duchai2501 <99119719+duchai2501@users.noreply.github.com>
2022-12-09 00:39:39 +0700

    Update Student.java

--- Assignment/src/java/model/Student.java
+++ Assignment/src/java/model/Student.java
@@ -4,10 +4,86 @@
  */
 package model;
 
+import java.sql.Date;
+import java.util.ArrayList;
+
 /**
  *
  * @author admin
  */
 public class Student {
-    
+
+    private String id;
+    private String name;
+    private boolean gender;
+    private Date dob;
+    private String email;
+    private String image;
+    private ArrayList<Group> groups = new ArrayList<>();
+    private ArrayList<Attendance> attendance = new ArrayList<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public ArrayList<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(ArrayList<Group> groups) {
+        this.groups = groups;
+    }
+
+    public ArrayList<Attendance> getAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(ArrayList<Attendance> attendance) {
+        this.attendance = attendance;
+    }
+
 }
