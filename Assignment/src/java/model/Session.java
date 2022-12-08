56a317b485ca375c1af3cd29b974b6c43ce6677b
duchai2501 <99119719+duchai2501@users.noreply.github.com>
2022-12-09 00:35:46 +0700

    Update Session.java

--- Assignment/src/java/model/Session.java
+++ Assignment/src/java/model/Session.java
@@ -4,10 +4,95 @@
  */
 package model;
 
+import java.sql.Date;
+import java.util.ArrayList;
+
 /**
  *
  * @author admin
  */
 public class Session {
-    
+
+    private int id;
+    private Group group;
+    private Lecturer lecturer;
+    private Room room;
+    private TimeSlot slot;
+    private Date date;
+    private int index;
+    private boolean attanded;
+    private ArrayList<Attendance> attendance = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Lecturer lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public TimeSlot getSlot() {
+        return slot;
+    }
+
+    public void setSlot(TimeSlot slot) {
+        this.slot = slot;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public boolean isAttanded() {
+        return attanded;
+    }
+
+    public void setAttanded(boolean attanded) {
+        this.attanded = attanded;
+    }
+
+    public ArrayList<Attendance> getAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(ArrayList<Attendance> attendance) {
+        this.attendance = attendance;
+    }
+
 }
